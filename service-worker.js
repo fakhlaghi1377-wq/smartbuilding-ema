11447c4bb2fc929aed426efef6fe5f3802e33820
fakhlaghi1377-wq <fakhlaghi1377@gmail.com>
2026-07-25 10:46:06 +0330

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE = "smartbuilding-surveys-v1";
+const ASSETS = [
+    "./survey-config.js",
+    "./cloud-survey.js",
+    "./daily/",
+    "./daily/daily.html",
+    "./daily/daily.css",
+    "./daily/daily.js",
+    "./ema/",
+    "./ema/index.html",
+    "./ema/survey.css",
+    "./ema/survey.js",
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.method !== "GET") return;
+    event.respondWith(
+        fetch(event.request)
+            .then((response) => {
+                const copy = response.clone();
+                caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+                return response;
+            })
+            .catch(() => caches.match(event.request))
+    );
+});
